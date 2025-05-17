9c3a20a12718a02ede15e065cb3013b2dd559aa0
javlonbek556 <javlonzdess@gmail.com>
2025-05-17 14:37:49 +0500

    book resource in this folder

---
+++ app/Http/Resources/BookResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BookResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'title' => $this->title,
+             'description' => $this->description,
+             'price' => $this->price,
+            'author' => new AuthorResource($this->whenLoaded('user')),
+        
+        ];
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
