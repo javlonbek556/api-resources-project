1a02f92c64191d3f652384714bf9e08d773dee8c
javlonbek556 <javlonzdess@gmail.com>
2025-05-17 14:36:37 +0500

    author controller in here

---
+++ app/Http/Resources/AuthorResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use App\Http\Resources\BookResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AuthorResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'name' => $this->name,
+            'bio'=> $this->bio,
+            'books' => BookResource::collection($this->whenLoaded('books')),
+        ];
+    }
+    public function books()
+    {
+        return $this->hasMany(BookResource::class);
+    }
+}
